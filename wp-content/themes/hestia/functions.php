4869117cb03442442b50b08bae9f76259ba1b174
Gustavo Pioto <guspioto@gmail.com>
2017-06-19 23:25:55 -0300

    munei20170619-2

--- wp-content/themes/hestia/functions.php
+++ wp-content/themes/hestia/functions.php
@@ -786,7 +786,7 @@ function my_updated_messages( $messages ) {
     7 => __('Product saved.'),
     8 => sprintf( __('Product submitted. <a target="_blank" href="%s">Preview product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
     9 => sprintf( __('Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview product</a>'), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
-    10 => sprintf( __('Product draft updated. <a target="_blank" href="%s">Preview product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    10 => sprintf( __('Rascunho atualizado. <a target="_blank" href="%s">Preview product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
   );
   return $messages;
 }
